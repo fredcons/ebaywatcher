df6d8ea23646557e3e473f088d8bbac20c972652
Fred Cons <frederic.cons@gmail.com>
2018-10-21 21:42:59 +0200

    Renaming executable file

--- ebaywatcher.py
+++ ebaywatcher.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-from services import MailFormatter, SearchDefinitionLoader, ConfigLoader, MailSender, DateFormatter
+from services import MailFormatter, SearchDefinitionLoader, ConfigLoader, MailSender, DateFormatter, ConsolePrinter
 from search import Search
 import click
 
@@ -9,7 +9,7 @@ import click
 @click.option('--config_path', default='config/config.yaml', help='The path to the config file')
 @click.option('--search_definitions_path', default='config/search_definitions.yaml', help='The path to the search definitions file')
 @click.option('--num_days', default=1, help='The number of days to get back in time')
-@click.option('--mail/--no-mail', default=True)
+@click.option('--mail/--no-mail', default=True, help="Whether to send an email or not. If not, results wil be printed to the console")
 def run_search(config_path, search_definitions_path, num_days, mail):
 
     print("Running searches defined in %s, going %s days back in time, with %s" % (search_definitions_path, num_days, config_path))
@@ -23,14 +23,14 @@ def run_search(config_path, search_definitions_path, num_days, mail):
     start_date = DateFormatter.get_start_date(num_days)
 
     search = Search()
-    all_results = search.search(start_date, search_definitions)
+    results = search.search(start_date, search_definitions)
 
     if mail:
-        print("Sending mail")
-        mail = MailFormatter.format_results(all_results)
+        mail_content = MailFormatter.format_results(results)
         mail_sender = MailSender(config.email_config)
-        mail_sender.send_mail(mail)
-
+        mail_sender.send_mail(mail_content)
+    else:
+        ConsolePrinter.print_results(results)
 
 if __name__ == '__main__':
     run_search()
